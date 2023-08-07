5abc563c5f36acb29d695d17d7f49943ec5be606
Gabriel Aron Arias <aron9377@gmail.com>
2023-08-07 16:08:13 -0300

    creacion del formulario, agrendo los input necesarios

--- JuegaFulboFront/src/assets/pages/Contacto.jsx
+++ JuegaFulboFront/src/assets/pages/Contacto.jsx
@@ -46,3 +46,84 @@ const Contacto = () => {
       })
       .catch((error) => console.error(error));
   };
+
+  return (
+    <>
+      <h1 className="color-letra lta">Contactanos</h1>
+      <div className="container-fluid d-flex gap-5">
+        <figure className="col-6 d-none d-lg-block">
+          <video
+            muted
+            loop
+            autoPlay
+            className="videoContacto"
+            src="src\video\VideoPateandoP.mp4"
+            type="video/mp4"
+          ></video>
+        </figure>
+        <div className="formulariocss col-12 col-lg-4">
+          <Form
+            className="container-fluid "
+            ref={refForm}
+            action=""
+            onSubmit={handleSubmit}
+            required
+          >
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <fieldset className="field-name">
+                <Form.Label className="color-letra">Nombre completo</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="JuegaFulbo"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  required
+                />
+              </fieldset>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <fieldset className="field-name">
+                <Form.Label className="color-letra">Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="juegafulbo@gmail.com"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                />
+              </fieldset>
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <fieldset className="field-name">
+                <Form.Label className="color-letra">
+                  Ingrese el texto
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="ingrese el texto aqui"
+                  rows={3}
+                  name="message"
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  required
+                />
+              </fieldset>
+            </Form.Group>
+            <button className="color-letra d-flex btn btn-dark">Enviar</button>
+          </Form>
+        </div>
+
+        <figure className="messibanner d-none d-lg-block">
+          <img src="src\video\messiHumoo.gif" alt="messi" />
+        </figure>
+      </div>
+    </>
+  );
+};
+
+export default Contacto;
