d1326a0466482bfd0e791bd817586ed8e4e1c98b
Gabriel Aron Arias <aron9377@gmail.com>
2023-08-07 16:03:20 -0300

    funcion para poder recibir los email de los clientes

--- JuegaFulboFront/src/assets/pages/Contacto.jsx
+++ JuegaFulboFront/src/assets/pages/Contacto.jsx
@@ -16,3 +16,33 @@ const Contacto = () => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const serviceId = "service_xoa5gvn";
+    const templateId = "template_am23jln";
+    const apikey = "lv2I2YINRkFxOE99P";
+
+    emailjs
+      .sendForm(serviceId, templateId, refForm.current, apikey)
+      .then((result) => {
+        console.log(result);
+
+        setFormData({
+          username: "",
+          email: "",
+          message: "",
+        });
+
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "formulario enviado",
+          showConfirmButton: false,
+          timer: 2500,
+          color: "white",
+          background: "black",
+        });
+      })
+      .catch((error) => console.error(error));
+  };
