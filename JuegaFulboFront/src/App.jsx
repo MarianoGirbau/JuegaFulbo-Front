2d06098ee974b9014d8a0d84ed65c094f58633d4
Francesca <francescafacia@gmail.com>
2023-08-08 16:15:25 -0300

    agregando componente en pagina SobreNosotros

--- JuegaFulboFront/src/App.jsx
+++ JuegaFulboFront/src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Navigate } from "react-router-dom";
-import SobreNosotros from "./SobreNosotros";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      {/* <Routes>
-        <Route path="/sobreNosotros" element={<SobreNosotros.jsx />}></Route>
-      </Routes> */}
-      <SobreNosotros></SobreNosotros>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- JuegaFulboFront/src/SobreNosotros.jsx
+++ JuegaFulboFront/src/SobreNosotros.jsx
@@ -1,7 +1,50 @@
+import CardComponent from "./componentes/CardComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function SobreNosotros() {
   return (
-    <>
-      <div>sobre nosotros</div>
-    </>
+    <div className="d-flex flex-wrap   justify-content-center align-items-center">
+      <CardComponent
+        nombre="Cesar Mauricio Broll"
+        foto="https://i.pinimg.com/736x/49/15/a7/4915a78e689b61bba589ca1788b77f01.jpg"
+        descripcion="descripcion  descripcion descri djfnsdkjfnsdkjfnk jnfksjdnfk sjdnf kjndspcion"
+        github="https://github.com/CMBroll"
+        linkedin="https://www.linkedin.com/in/mauricio-broll/"
+      ></CardComponent>
+      <CardComponent
+        nombre="Gabriel Aron Arias"
+        foto="https://i.pinimg.com/736x/49/15/a7/4915a78e689b61bba589ca1788b77f01.jpg"
+        descripcion="descripcion  descripcion descripcion"
+        github="https://github.com/Aron9377"
+        linkedin="www.linkedin.com/in/gabriel-aron-arias"
+      ></CardComponent>
+      <CardComponent
+        nombre="María Agustina González Gauna"
+        foto="https://i.pinimg.com/736x/49/15/a7/4915a78e689b61bba589ca1788b77f01.jpg"
+        descripcion="descripcion  descripcion descripcion"
+        github="https://github.com/MariaAgustinaGonzalezGauna"
+        linkedin="https://www.linkedin.com/in/maria-agustina-gonzalez-gauna/"
+      ></CardComponent>
+      <CardComponent
+        nombre="Francesca Falci Antacle"
+        foto="https://i.pinimg.com/736x/49/15/a7/4915a78e689b61bba589ca1788b77f01.jpg"
+        descripcion="descripcion  descripcion descripcion"
+        github="https://github.com/FranceFalci"
+        linkedin="https://www.linkedin.com/in/francesca-falci-antacle-8a0269245"
+      ></CardComponent>
+      <CardComponent
+        nombre="Mariano Girbau"
+        foto="https://i.pinimg.com/736x/49/15/a7/4915a78e689b61bba589ca1788b77f01.jpg"
+        descripcion="descripcion  descripcion descripcion"
+        github="https://github.com/MarianoGirbau"
+        linkedin="https://www.linkedin.com/in/mariano-girbau-b25868219/"
+      ></CardComponent>
+      <CardComponent
+        nombre="Antonella Banchero"
+        foto="https://i.pinimg.com/736x/49/15/a7/4915a78e689b61bba589ca1788b77f01.jpg"
+        descripcion="descripcion  descripcion descripcion"
+        github="https://github.com/AntoBanchero"
+        linkedin="www.linkedin.com/in/antonella-banchero"
+      ></CardComponent>
+    </div>
   );
 }
