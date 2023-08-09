9b90066af4319544adcd5c1bd1e2bf8ec1d88def
CMBroll <mbroll25@gmail.com>
2023-08-09 12:54:31 -0300

    Se agrego mensaje de Error, imagen y se le agrego responsividad

--- JuegaFulboFront/src/componentes/pages/Error404.jsx
+++ JuegaFulboFront/src/componentes/pages/Error404.jsx
@@ -3,8 +3,23 @@ import "./error404.css";
 function Error404() {
   return (
     <>
-      <main className="container-fluid vh-100 bgFut">
-
+      <main className="container-fluid bgFut d-flex">
+        <section className="container contPrincipal d-flex justify-content-center align-items-center">
+          <section className="col d-flex d-none d-lg-block">
+            <div className="relativo">
+            <img src="/img/FDsombra.png" alt="" className="absoluto" />
+            </div>
+          </section>
+          <section className="col d-flex flex-column justify-content-center text-center">
+            <h1 className="d-none d-lg-block h1Large">ERROR 404</h1>
+            <h1 className="d-sm-block d-lg-none h1Small">ERROR 404</h1>
+            <p className="p-4">
+              Lo sentimos, la página que estás buscando no se encuentra
+              disponible. Ponete la camiseta y volve a la cancha!
+            </p>
+            <button className="p-4">VOLVER AL INICIO</button>
+          </section>
+        </section>
       </main>
     </>
   );
