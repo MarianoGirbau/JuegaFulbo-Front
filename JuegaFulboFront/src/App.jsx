ac042748f2c8d0ecda0d231d0ca7703cde52387a
Agus <maria.aguss.ag@gmail.com>
2023-08-09 23:04:36 -0300

    footer agregado

--- JuegaFulboFront/src/App.jsx
+++ JuegaFulboFront/src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import PaginaInicial from "./componentes/pages/inicio";
+import Footer from "./componentes/pages/Footer"
 
 function App() {
 
   return (
     <>
       <PaginaInicial />
+      <Footer />
     </>
   )
 }
