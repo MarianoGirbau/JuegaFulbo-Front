6851e0bdee8e2987416a52ffc764766434e95050
MarianoGirbau <marianogirbau@gmail.com>
2023-08-14 22:05:16 -0300

    Cambio de home por inicio

--- JuegaFulboFront/src/componentes/pages/home/Inicio.jsx
+++ JuegaFulboFront/src/componentes/pages/home/Inicio.jsx
@@ -6,12 +6,12 @@ function PaginaInicial() {
       <div className="container-fluid d-flex centrado flex-column justify-content-center align-items-center webLengEfecto">
         <h3 className="h2Title text-center">¿QUE BUSCAS?</h3>
         <div className="d-flex gap-5 flex-wrap justify-content-center">
-        <div className="cuadroCanchas d-flex justify-content-center align-items-center" >
-          <h3 className="h3Title"><a href="LARUTAACA">CANCHAS</a></h3>
-        </div>
-        <div className="cuadroQuinchos d-flex justify-content-center align-items-center">
-          <h3 className="h3Title"><a href="LARUTAACA">QUINCHOS</a></h3>
-        </div>
+        <a href="/cancha" className="cuadroCanchas d-flex justify-content-center align-items-center" >
+          <h3 className="h3Title">CANCHAS</h3>
+        </a>
+        <a href="/quincho" className="cuadroQuinchos d-flex justify-content-center align-items-center">
+          <h3 className="h3Title">QUINCHOS</h3>
+        </a>
         </div>
       </div>
     </>
--- JuegaFulboFront/src/componentes/rutas/Rutas.jsx
+++ JuegaFulboFront/src/componentes/rutas/Rutas.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Contacto from "../pages/Contacto/Contacto.jsx"
-import Home from "../pages/home/Home.jsx";
+import Inicio from "../pages/home/Inicio.jsx";
 import RegistroForm from "../pages/registro/RegistroForm.jsx";
 
 const Rutas = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/" element={<Inicio/>} />
             <Route path="/contacto" element={<Contacto/>} />
             <Route path="/registro" element={<RegistroForm/>} />
         </Routes>
