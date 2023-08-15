1cc0da3267d17a3e6e08211dd746d6a1fb6972af
CMBroll <mbroll25@gmail.com>
2023-08-15 09:39:08 -0300

    Fix respinsive Registro y cambio de rutas de las imagenes

--- JuegaFulboFront/src/componentes/pages/registro/RegistroForm.jsx
+++ JuegaFulboFront/src/componentes/pages/registro/RegistroForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./RegistroForm.css";
-import fulboRegistro from "/img/fulboRegistro.jpg";
-import { Container, Row, Col } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -58,104 +57,108 @@ const RegistroForm = () => {
 
   return (
     <>
-        <Container id="container-registro">
-          <Row>
-            <Col sm={12} md={12} lg={7}>
-              <Form id="form-registro" onSubmit={handleSubmit}>
-                <h5 className="titulo_registro">Formulario de registro</h5>
-                <Form.Group>
-                  <Form.Label className="subtitulos_registro">
-                    Nombre
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    id="nombre"
-                    value={nombre}
-                    minLength={8}
-                    maxLength={15}
-                    placeholder="Ingrese su nombre"
-                    onChange={(e) => setNombre(e.target.value)}
-                    required
-                  />
-                  {errors.nombre && <span>{errors.nombre}</span>}
-                </Form.Group>
+      <main className="my-5 d-flex flex-column align-items-center justify-content-center mx-4">
+        <div className="logoFulbo d-block d-md-none">
+          <img src="/img/LogoJuegaFulbo.svg" alt="" className="pb-5" />
+        </div>
+        <Container fluid className="custom-container row borderCont overflow-hidden p-0">
+          <Col xs={12} md={7} className="contIqz pb-2 px-5 ">
+            <div className="volverAtras d-flex gap-3 align-items-center pt-3">
+              <img src="/img/flechaIzq.svg" alt="" />
+              <h5>Volver atrás</h5>
+            </div>
+            <hr />
+            <h4>Formulario de registro</h4>
 
-                <Form.Group className="">
-                  <Form.Label className="subtitulos_registro">
-                    Apellido
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    id="apellido"
-                    minLength={8}
-                    maxLength={15}
-                    placeholder="Ingrese su apellido"
-                    value={apellido}
-                    onChange={(e) => setApellido(e.target.value)}
-                    required
-                  />
-                  {errors.apellido && <span>{errors.apellido}</span>}
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label className="subtitulos_registro">Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    id="email"
-                    value={email}
-                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-                    minLength={10}
-                    maxLength={30}
-                    placeholder="Ingrese su email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                  {errors.email && <span>{errors.email}</span>}
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label className="subtitulos_registro">
-                    Contraseña
-                  </Form.Label>
-                  <Form.Control
-                    type="password"
-                    id="password"
-                    value={password}
-                    minLength={8}
-                    maxLength={30}
-                    placeholder="Ingrese su contraseña"
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                  {errors.password && <span>{errors.password}</span>}
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label className="subtitulos_registro">
-                    Confirmar contraseña
-                  </Form.Label>
-                  <Form.Control
-                    type="password"
-                    id="confirmPassword"
-                    minLength={8}
-                    maxLength={30}
-                    placeholder="Ingrese nuevamente su contraseña"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    required
-                  />
-                  {errors.confirmPassword && (
-                    <span>{errors.confirmPassword}</span>
-                  )}
-                </Form.Group>
+            <Form id="form-registrooo" onSubmit={handleSubmit}>
+              <Form.Group className="mb-2">
+                <Form.Label className="gruppoFont">Nombre</Form.Label>
+                <Form.Control
+                  type="text"
+                  id="nombre"
+                  className="custom-form-control custom-bg-dark"
+                  value={nombre}
+                  minLength={8}
+                  maxLength={15}
+                  placeholder="Ingrese su nombre"
+                  onChange={(e) => setNombre(e.target.value)}
+                  required
+                />
+                {errors.nombre && <span>{errors.nombre}</span>}
+              </Form.Group>
 
-                <Button variant="black" id="botonLog" type="submit">
-                  <h4>ENVIAR</h4>
+              <Form.Group className="mb-2">
+                <Form.Label className="gruppoFont">Apellido</Form.Label>
+                <Form.Control
+                  type="text"
+                  id="apellido"
+                  className="custom-form-control custom-bg-dark"
+                  minLength={8}
+                  maxLength={15}
+                  placeholder="Ingrese su apellido"
+                  value={apellido}
+                  onChange={(e) => setApellido(e.target.value)}
+                  required
+                />
+                {errors.apellido && <span>{errors.apellido}</span>}
+              </Form.Group>
+              <Form.Group className="mb-2">
+                <Form.Label className="gruppoFont">
+                  Correo Electrónico
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  id="email"
+                  className="custom-form-control custom-bg-dark"
+                  value={email}
+                  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                  minLength={10}
+                  maxLength={30}
+                  placeholder="Ingrese su email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                {errors.email && <span>{errors.email}</span>}
+              </Form.Group>
+              <Form.Group className="mb-2">
+                <Form.Label className="gruppoFont">Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  id="confirmPassword"
+                  className="custom-form-control custom-bg-dark"
+                  minLength={8}
+                  maxLength={30}
+                  placeholder="Ingrese nuevamente su contraseña"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+                {errors.confirmPassword && (
+                  <span>{errors.confirmPassword}</span>
+                )}
+              </Form.Group>
+
+              <div className="my-4 bordeBoton">
+                <Button
+                  variant="black"
+                  id="botonLog"
+                  type="submit"
+                  className="w-100"
+                >
+                  <h4 className="m-0 py-3">ENVIAR</h4>
                 </Button>
-              </Form>
-            </Col>
-            <Col lg={5} className="d-none d-lg-block">
-              <img className="imgRegistro" src={fulboRegistro} alt="" />
-            </Col>
-          </Row>
+              </div>
+            </Form>
+          </Col>
+
+          <Col
+            xs={12}
+            sm={0}
+            md={5}
+            className="contDer overflow-hidden d-none d-sm-block"
+          ></Col>
         </Container>
+      </main>
     </>
   );
 };
