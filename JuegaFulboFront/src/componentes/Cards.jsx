b70798c609504fc26da122654465f4fea7ec3aac
CMBroll <mbroll25@gmail.com>
2023-08-20 09:03:32 -0300

    Agregando responsividad

--- JuegaFulboFront/src/componentes/Cards.jsx
+++ JuegaFulboFront/src/componentes/Cards.jsx
@@ -1,29 +1,201 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import "./Cards.css"
+import "./cards.css";
 
 function Cards() {
   return (
     <>
-      <Card  style={{ width: "25rem" }}>
-        <Card.Img variant="top" src="/img/futbol.jpg" />
-        <Card.Body className="d-flex flex-column text-center justify-content-center">
-        <div className="d-flex flex-row justify-content-between">
-            <Card.Text>
-            <svg className="me-2" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M435.4 361.3l-89.7-6c-5.2-.3-10.3 1.1-14.5 4.2s-7.2 7.4-8.4 12.5l-22 87.2c-14.4 3.2-29.4 4.8-44.8 4.8s-30.3-1.7-44.8-4.8l-22-87.2c-1.3-5-4.3-9.4-8.4-12.5s-9.3-4.5-14.5-4.2l-89.7 6C61.7 335.9 51.9 307 49 276.2L125 228.3c4.4-2.8 7.6-7 9.2-11.9s1.4-10.2-.5-15L100.4 118c19.9-22.4 44.6-40.5 72.4-52.7l69.1 57.6c4 3.3 9 5.1 14.1 5.1s10.2-1.8 14.1-5.1l69.1-57.6c27.8 12.2 52.5 30.3 72.4 52.7l-33.4 83.4c-1.9 4.8-2.1 10.1-.5 15s4.9 9.1 9.2 11.9L463 276.2c-3 30.8-12.7 59.7-27.6 85.1zM256 48l.9 0h-1.8l.9 0zM56.7 196.2c.9-3 1.9-6.1 2.9-9.1l-2.9 9.1zM132 423l3.8 2.7c-1.3-.9-2.5-1.8-3.8-2.7zm248.1-.1c-1.3 1-2.7 2-4 2.9l4-2.9zm75.2-226.6l-3-9.2c1.1 3 2.1 6.1 3 9.2zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm14.1-325.7c-8.4-6.1-19.8-6.1-28.2 0L194 221c-8.4 6.1-11.9 16.9-8.7 26.8l18.3 56.3c3.2 9.9 12.4 16.6 22.8 16.6h59.2c10.4 0 19.6-6.7 22.8-16.6l18.3-56.3c3.2-9.9-.3-20.7-8.7-26.8l-47.9-34.8z"/></svg>
-                F5
-            </Card.Text>
-            <Card.Title>"Cancha X"</Card.Title>
-            <Card.Text>
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M160 0c17.7 0 32 14.3 32 32V67.7c1.6 .2 3.1 .4 4.7 .7c.4 .1 .7 .1 1.1 .2l48 8.8c17.4 3.2 28.9 19.9 25.7 37.2s-19.9 28.9-37.2 25.7l-47.5-8.7c-31.3-4.6-58.9-1.5-78.3 6.2s-27.2 18.3-29 28.1c-2 10.7-.5 16.7 1.2 20.4c1.8 3.9 5.5 8.3 12.8 13.2c16.3 10.7 41.3 17.7 73.7 26.3l2.9 .8c28.6 7.6 63.6 16.8 89.6 33.8c14.2 9.3 27.6 21.9 35.9 39.5c8.5 17.9 10.3 37.9 6.4 59.2c-6.9 38-33.1 63.4-65.6 76.7c-13.7 5.6-28.6 9.2-44.4 11V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V445.1c-.4-.1-.9-.1-1.3-.2l-.2 0 0 0c-24.4-3.8-64.5-14.3-91.5-26.3c-16.1-7.2-23.4-26.1-16.2-42.2s26.1-23.4 42.2-16.2c20.9 9.3 55.3 18.5 75.2 21.6c31.9 4.7 58.2 2 76-5.3c16.9-6.9 24.6-16.9 26.8-28.9c1.9-10.6 .4-16.7-1.3-20.4c-1.9-4-5.6-8.4-13-13.3c-16.4-10.7-41.5-17.7-74-26.3l-2.8-.7 0 0C119.4 279.3 84.4 270 58.4 253c-14.2-9.3-27.5-22-35.8-39.6c-8.4-17.9-10.1-37.9-6.1-59.2C23.7 116 52.3 91.2 84.8 78.3c13.3-5.3 27.9-8.9 43.2-11V32c0-17.7 14.3-32 32-32z"/></svg>
-                4000
-            </Card.Text>
-        </div>
-          <Button variant="dark">Reservar</Button>
-        </Card.Body>
-      </Card>
+      <body className="bg-canchas">
+        <main className="container-fluid container-custom">
+          <div className="container-fluid seleccion-canchas">
+            <div className="row pt-4 mb-3">
+              <div className="col-12 col-md-4 d-flex justify-content-center py-2">
+                <button className="w-100 py-2">Futbol 5</button>
+              </div>
+              <div className="col-12 col-md-4 d-flex justify-content-center py-2">
+                <button className="w-100 py-2">Futbol 7</button>
+              </div>
+              <div className="col-12 col-md-4 d-flex justify-content-center py-2">
+                <button className="w-100 py-2">Futbol 11</button>
+              </div>
+            </div>
+          </div>
+          <div className="linea-divisora"></div>
+          <div className="row botones-custom">
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha1.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 5</p>
+                  <p className="m-0">Cancha 1</p>
+                  <p className="m-0">$4000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha2.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 7</p>
+                  <p className="m-0">Cancha 2</p>
+                  <p className="m-0">$6000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha3.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 5</p>
+                  <p className="m-0">Cancha 3</p>
+                  <p className="m-0">$4000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha4.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 7</p>
+                  <p className="m-0">Cancha 4</p>
+                  <p className="m-0">$5000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha5.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 5</p>
+                  <p className="m-0">Cancha 5</p>
+                  <p className="m-0">$4000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha6.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 11</p>
+                  <p className="m-0">Cancha 6</p>
+                  <p className="m-0">$6500</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha7.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 11</p>
+                  <p className="m-0">Cancha 7</p>
+                  <p className="m-0">$6000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha8.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 5</p>
+                  <p className="m-0">Cancha 8</p>
+                  <p className="m-0">$4000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-4">
+              <section className="cards">
+                <div className="img-bg">
+                  <img
+                    src="/img/Cancha9.jpg"
+                    className="img-fluid cover-image"
+                    alt=""
+                  />
+                </div>
+                <div className="d-flex justify-content-between px-3 py-2">
+                  <p className="m-0">Futbol 7</p>
+                  <p className="m-0">Cancha 9</p>
+                  <p className="m-0">$5000</p>
+                </div>
+                <section className="boton-reserva d-flex justify-content-center mb-3">
+                  <button className="b-custom w-100">Reservar</button>
+                </section>
+              </section>
+            </div>
+          </div>
+        </main>
+      </body>
     </>
   );
 }
 
-export default Cards
+export default Cards;
