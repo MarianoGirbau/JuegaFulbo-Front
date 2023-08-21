8c420683c600e738aaf0daa96216f594b0edf895
CMBroll <mbroll25@gmail.com>
2023-08-20 22:22:10 -0300

    Filtro reservas

--- JuegaFulboFront/src/componentes/Cards.jsx
+++ JuegaFulboFront/src/componentes/Cards.jsx
@@ -1,15 +1,79 @@
 import Card from "./Card";
 import "./cards.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CanchasContext } from "../context/CanchasContext";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+import { registerLocale } from 'react-datepicker';
+import es from 'date-fns/locale/es';
+
+
+registerLocale('es', es);
+
 
 function Cards() {
+
   const { canchas } = useContext(CanchasContext);
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
     <>
       <div className="bg-canchas">
         <main className="container-fluid container-custom">
-          <div className="container-fluid seleccion-canchas">
+
+
+
+          <div className="container-fluid">
+            <div className="row mb-3 pt-4 buscador">
+              <div className="col-sm-12 col-md-4 col-lg-3 pb-3">
+                <label htmlFor="opciones1"></label>
+                <select id="opciones1" className="w-100 custom-select">
+                  <option disabled selected value="">
+                    Tipo de Futbol
+                  </option>
+                  <option>Futbol 5</option>
+                  <option>Futbol 7</option>
+                  <option>Futbol 11</option>
+                </select>
+              </div>
+
+              <div className="col-sm-12 col-md-4 col-lg-3 pb-3">
+                <label htmlFor="opciones3"></label>
+                <select id="opciones3" className="w-100 custom-select">
+                  <option disabled selected value="">
+                    Seleccione horario
+                  </option>
+                  <option>10:00hs</option>
+                  <option>12:00hs</option>
+                  <option>14:00hs</option>
+                  <option>14:00hs</option>
+                  <option>16:00hs</option>
+                  <option>18:00hs</option>
+                  <option>20:00hs</option>
+                </select>
+              </div>
+
+              <div className="col-sm-12 col-md-4 col-lg-3 pb-3">
+                <label htmlFor="opciones2"></label>
+                <DatePicker
+                  id="opciones2"
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  className="custom-select"
+                  dateFormat={"dd MMM yyyy"}
+                  minDate={new Date()}
+                  locale="es"
+                  placeholderText="Ingrese la fecha"
+                />
+              </div>
+
+              <div className="col-sm-12 col-md-12 col-lg-3 pb-3">
+                <button className="w-100 boton-reserva">Buscar</button>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="container-fluid seleccion-canchas">
             <div className="row pt-4 mb-3">
               <div className="col-12 col-md-4 d-flex justify-content-center py-2">
                 <button className="w-100 py-2">Futbol 5</button>
@@ -21,7 +85,11 @@ function Cards() {
                 <button className="w-100 py-2">Futbol 11</button>
               </div>
             </div>
-          </div>
+          </div> */}
+
+
+
+
           <div className="linea-divisora"></div>
           <div className=" row botones-custom">
             {canchas.length > 0 ? (
