6605b6737ee72536a4168d79f46666d6ab850cb5
CMBroll <mbroll25@gmail.com>
2023-08-22 22:37:45 -0300

    Agregando padding en inicio para que no choque con footer

--- JuegaFulboFront/src/componentes/pages/home/Inicio.jsx
+++ JuegaFulboFront/src/componentes/pages/home/Inicio.jsx
@@ -3,7 +3,7 @@ import "./inicio.css";
 function PaginaInicial() {
   return (
     <>
-      <div className="container-fluid d-flex centrado flex-column justify-content-center align-items-center webLengEfecto">
+      <div className="container-fluid d-flex centrado flex-column justify-content-center align-items-center webLengEfecto pb-5">
         <h3 className="h2Title text-center">¿QUE BUSCAS?</h3>
         <div className="d-flex gap-5 flex-wrap justify-content-center">
           <a
