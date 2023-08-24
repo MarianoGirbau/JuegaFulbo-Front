96fba52b7c1991ff4c73a0dccbe735a209d967a1
CMBroll <mbroll25@gmail.com>
2023-08-23 22:58:15 -0300

    Dando estilo a Mis reservas

--- JuegaFulboFront/src/componentes/pages/reservas/MisReservas.jsx
+++ JuegaFulboFront/src/componentes/pages/reservas/MisReservas.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { CanchasContext } from "../../../context/CanchasContext";
+import "../reservas/MisReservas.css";
 
 export default function MisReservas() {
   const { obtenerReserva, MisReservas } = useContext(CanchasContext);
@@ -26,18 +27,38 @@ export default function MisReservas() {
 
   return (
     <>
-      <h1>Mis reservas</h1>
-      <ul>
-        {reservasGuardadas && reservasGuardadas.length > 0 ? (
-          reservasGuardadas.map((reserva, index) => (
-            <li key={index}>
-              Cancha {reserva.numero}, Día {reserva.dia}, Horario {reserva.horario}
-            </li>
-          ))
-        ) : (
-          <li>No tienes reservas guardadas.</li>
-        )}
-      </ul>
+      <div className="container-fluid custom-cont-reservas my-4">
+        <h1 className="text-center pt-3 sub-h1">Mis reservas</h1>
+        <hr />
+        <section className="d-flex flex-wrap justify-content-center gap-3">
+          {reservasGuardadas && reservasGuardadas.length > 0 ? (
+            reservasGuardadas.map((reserva, index) => (
+              <div key={index}>
+                <div className="boxes-reservas">
+                  <h2 className="sub-h2 text-center">Precio: ${reserva.precio}</h2>
+                  <hr />
+                  <div className="d-flex justify-content-between">
+                  <div className="d-flex flex-column justify-content align-items-center">
+                    <img src="/img/IconoCalendario.svg" width={"32px"} alt="" />
+                    <p className="m-0">Día {reserva.dia}</p>
+                  </div>
+                  <div className="d-flex flex-column align-items-center">
+                    <img src="/img/CanchaNumero.svg" width={"50px"} alt="" />
+                    <p className="m-0">Cancha {reserva.numero}</p>
+                  </div>
+                  <div className="d-flex flex-column align-items-center">
+                  <img src="/img/IconoReloj.svg" width={"50px"} alt="" />
+                    <p className="m-0 ">Horario {reserva.horario}</p>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <li>No tienes reservas guardadas.</li>
+          )}
+        </section>
+      </div>
     </>
   );
 }
