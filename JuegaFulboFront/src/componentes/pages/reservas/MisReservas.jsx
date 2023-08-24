69d5790ff98b2f4fb2d5d519dd13ce581bb1ae6b
Agus <maria.aguss.ag@gmail.com>
2023-08-23 21:32:05 -0300

    Arreglo a MisReservas

--- JuegaFulboFront/src/componentes/pages/reservas/MisReservas.jsx
+++ JuegaFulboFront/src/componentes/pages/reservas/MisReservas.jsx
@@ -2,24 +2,27 @@ import React, { useContext, useEffect, useState } from "react";
 import { CanchasContext } from "../../../context/CanchasContext";
 
 export default function MisReservas() {
-  const { MisReservas } = useContext(CanchasContext);
+  const { obtenerReserva, MisReservas } = useContext(CanchasContext);
   const usuario = JSON.parse(localStorage.getItem("usuario"));
   const [reservasGuardadas, setReservasGuardadas] = useState([]);
+  const idUsuario = usuario.id;
 
   useEffect(() => {
-    // Solo cargar desde el localStorage si no hay datos en reservasGuardadas
-    if (usuario && reservasGuardadas.length === 0) {
-      const idUsuario = usuario.id;
-      console.log(idUsuario, "usuario");
+    async function fetchReservas() {
+      if (usuario) {
+        console.log(idUsuario, "usuario");
 
-      try {
-        const reservas = JSON.parse(localStorage.getItem("reservasUsuario")) || [];
-        setReservasGuardadas(reservas);
-      } catch (error) {
-        console.error("Error al obtener las reservas:", error);
+        try {
+          const reservas = await obtenerReserva(idUsuario);
+          setReservasGuardadas(reservas);
+        } catch (error) {
+          console.error("Error al obtener las reservas:", error);
+        }
       }
     }
-  }, [usuario, reservasGuardadas]); // Aseguramos que se ejecute cuando usuario cambie o reservasGuardadas cambie
+
+    fetchReservas();
+  }, [obtenerReserva]);
 
   return (
     <>
